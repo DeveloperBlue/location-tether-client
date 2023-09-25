635422f4a4b6ae98f859282d50228c2e00fbdd8b
Michael Rooplall <MichaelRooplall@gmail.com>
2023-09-25 12:32:37 -0400

    Add background location polling

--- App.tsx
+++ App.tsx
@@ -3,24 +3,54 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider, Button } from 'react-native-paper';
 import * as Location from 'expo-location';
+import { LocationObject } from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+
+const TASK_NAME = 'LOCATION_TETHER'
+
+TaskManager.defineTask<{locations : LocationObject[]}>('LOCATION_TETHER', ({ data: { locations }, error }) => {
+	if (error) {
+		// check `error.message` for more details.
+		console.log(error.message);
+		return;
+	}
+	console.log('Received new locations [fg/bg]', locations);
+});
+
 
 const LocationHandler = () => {
 
-	const [location, setLocation] = useState<any | null>(null);
+	const [location, setLocation] = useState<LocationObject | null>(null);
 
 	useEffect(() => {
+
 		(async () => {
 		  
-			let { status } = await Location.requestForegroundPermissionsAsync();
-			if (status !== 'granted') {
-				console.log('Permission to access location was denied');
+			const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+			
+			if (foregroundStatus !== 'granted') {
+				console.log('Permission to access foreground location was denied');
 				return;
 			}
+
+			const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+			
+			if (backgroundStatus !== 'granted') {
+				console.log('Permission to access background location was denied');
+				return;
+			}
+
+			await Location.startLocationUpdatesAsync(TASK_NAME, {
+				accuracy: Location.Accuracy.Balanced,
+			});
 
 			let location = await Location.getCurrentPositionAsync({});
 			setLocation(location);
 
 		})();
+
+		Location.startLocationUpdatesAsync(TASK_NAME, {})
+
 	}, []);
 
 	return (
@@ -35,7 +65,7 @@ const LocationHandler = () => {
 			<Button
 				mode={'contained'}
 				onPress={async () => {
-					
+
 					let { status } = await Location.requestForegroundPermissionsAsync();
 					if (status !== 'granted') {
 						console.log('Permission to access location was denied');
