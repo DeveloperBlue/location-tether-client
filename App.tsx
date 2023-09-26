e2109770065e3976807a58e44eb9f8bf72a3c7d6
Michael Rooplall <MichaelRooplall@gmail.com>
2023-09-26 06:08:44 -0400

    Add device info libs

--- App.tsx
+++ App.tsx
@@ -1,26 +1,56 @@
 import { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider, Button } from 'react-native-paper';
+import { ColorValue, StyleSheet, Text, View } from 'react-native';
+import { PaperProvider, Button, Surface } from 'react-native-paper';
+
 import * as Location from 'expo-location';
 import { LocationObject } from 'expo-location';
 import * as TaskManager from 'expo-task-manager';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+
+import DeviceInfo from 'react-native-device-info';
 
 const TASK_NAME = 'LOCATION_TETHER'
 
+/* Sent updates to the service */
 TaskManager.defineTask<{locations : LocationObject[]}>('LOCATION_TETHER', ({ data: { locations }, error }) => {
 	if (error) {
 		// check `error.message` for more details.
 		console.log(error.message);
 		return;
 	}
-	console.log('Received new locations [fg/bg]', locations);
+	
+	if (locations.length == 0) return;
+
+	const topLocation = locations.sort((locationA, locationB) => {
+		return locationA.timestamp - locationB.timestamp;
+	})[0];
+
+	console.log('Received new location [foreground/background]', topLocation);
+
+	fetch('')
 });
 
+const CustomMarker = ({backgroundColor, outlineColor, size = 24} : {backgroundColor : ColorValue, outlineColor : ColorValue, size? : number}) => {
+	return (
+		<View
+			style={{
+				backgroundColor : backgroundColor,
+				width : size,
+				height : size,
+				borderRadius : size,
+				borderWidth : 2,
+				borderColor: outlineColor
+			}}
+		/>
+	)
+}
 
 const LocationHandler = () => {
 
+	const [initialLocation, setInitialLocation] = useState<LocationObject | null>(null);
 	const [location, setLocation] = useState<LocationObject | null>(null);
+	const [tetherLocation, setTetherLocation] = useState();
 
 	useEffect(() => {
 
@@ -45,16 +75,63 @@ const LocationHandler = () => {
 			});
 
 			let location = await Location.getCurrentPositionAsync({});
+			console.log('Get initial location on mount', location);
+			setInitialLocation(location);
 			setLocation(location);
 
 		})();
 
 		Location.startLocationUpdatesAsync(TASK_NAME, {})
 
+		Location.watchPositionAsync({}, (location) => {
+			console.log('Received new location [foreground]', location);
+			setLocation(location);
+		})
+
+		const deviceID = DeviceInfo.getDeviceId();
+		const deviceName = DeviceInfo.getDeviceNameSync();
+
+		// Poll for positional data
+		const interval = setInterval(() => {
+
+		}, 30 * 1000)
+
+		return (() => {
+			clearInterval(interval);
+		})
+
 	}, []);
 
 	return (
 		<View>
+			{
+				initialLocation && <MapView
+					provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+					style={styles.map}
+					initialRegion={{
+						latitude: initialLocation.coords.latitude,
+						longitude: initialLocation.coords.longitude,
+						latitudeDelta: 0.015,
+						longitudeDelta: 0.0121,
+					}}
+				>
+					{location &&
+						<Marker
+							key={'Device'}
+							title={'Device'}
+							description={'This device'}
+							coordinate={{
+								latitude : location.coords.latitude,
+								longitude: location.coords.longitude
+							}}
+						>
+							<CustomMarker backgroundColor={'#fff'} outlineColor={'#000'}/>
+						</Marker>
+					}
+					
+				</MapView>
+			}
+			
 			{
 				location ? <Text>
 					{JSON.stringify(location)}
@@ -102,5 +179,10 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		padding : 16,
+		gap : 10
 	},
+	map: {
+		height: 220,
+	}
 });
